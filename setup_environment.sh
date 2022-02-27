ec84f2620f796437ed02b6c6c0eb72eceebdb919
Elliott Ash <ashe@ethz.ch>
2022-02-27 13:07:28 +0100

    modified:   homework/homework_08.ipynb 	modified:   notebooks/01_intro-text.ipynb 	new file:   slides/02_tokens.pdf

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,18 +1,18 @@
-conda create -n legal_dna python=3.6
-conda activate legal_dna
+conda create -n nlp_lss python=3.8
+conda activate nlp_lss
 
-pip install spacy==2.3.5
-pip install pandas==1.1.5
-pip install tqdm==4.56.0
-pip install gensim==3.8.3
-pip install beautifulsoup4==4.9.3
+pip install spacy==3.2.2
+pip install pandas==1.4.1
+pip install tqdm==4.62.3
+pip install gensim==4.1.2
+pip install beautifulsoup4==4.10.0
 pip install googletrans==3.0.0
-pip install matplotlib==3.3.4
+pip install matplotlib==3.5.1
 pip install spacytextblob
-pip install lxml==4.6.2
+pip install lxml==4.8.0
 pip install transformers[torch]
-pip install unidecode==1.2.0
-pip install seaborn==0.11.1
+pip install unidecode==2.8
+pip install seaborn==0.11.2
 pip install sklearn==0.24.1
 pip install wordcloud==1.8.1
 pip install fightin-words==1.0.5
@@ -20,9 +20,7 @@ pip install eli5==0.11.0
 pip install flair==0.7.0
 pip install tensorflow==2.4.1
 pip install keras==2.4.3
-pip install transformers
 pip install sentencepiece==0.1.91
-pip install transformers==3.5.1
 pip install scikit-learn-extra==0.1.0b2
 pip install pyldavis==3.2.2
 pip install hdbscan==0.8.27
@@ -39,8 +37,6 @@ python -c "import nltk; nltk.download('vader_lexicon')"
 
 # install jupyter notebook
 conda install -c conda-forge jupyterlab
-
-python -m spacy download en_core_web_sm
 
 
 sudo apt install graphviz
